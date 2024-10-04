e5caa9277dac38c3d3804e22e11f0937a0e451f9
Alejandro Vázquez Yerga <vazquez.yerga.alejandro@iescamas.es>
2024-10-04 09:40:28 +0200

    Actualizada la base de datos

---
+++ bbdd/libros.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE libreria;
+
+USE libreria;
+CREATE TABLE libros(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT,
+    precio DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL,
+    
+)
+
+INSERT INTO libros(nombre,descripcion,precio,stock) VALUES
+('Luna de Plutón', 'Libro de ciencia ficción',22.50 ,30),
+('Don Quijote', 'Novela caballeresca', 17.50, 50),
+('El último trabajo del Señor Luna','Libro de suspense', 20.00, 15);

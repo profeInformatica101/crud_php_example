f83764c608ac286a45da9cf9bee8c7507eebede1
David Bejarano Llano <sonicorn.db@gmail.com>
2024-10-04 09:28:32 +0200

    Corrección autor

--- bbdd/libros.sql
+++ bbdd/libros.sql
@@ -4,6 +4,7 @@ USE libreria /*tienda*/;
 CREATE TABLE libros /*jabones*/(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10, 2) NOT NULL,
     stock INT NOT NULL
